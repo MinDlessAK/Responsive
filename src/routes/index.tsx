b246096e000e2e82e442807a4728ca033e6e96e5
MinDlessAK <MinDlessAK@users.noreply.github.com>
2023-07-09 12:01:32 +0530

    tsx

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,112 +1,78 @@
-import { component$ } from "@builder.io/qwik";
-import type { DocumentHead } from "@builder.io/qwik-city";
-
-import Counter from "~/components/starter/counter/counter";
-import Hero from "~/components/starter/hero/hero";
-import Infobox from "~/components/starter/infobox/infobox";
-import Starter from "~/components/starter/next-steps/next-steps";
-
-export default component$(() => {
+export default () => {
   return (
     <>
-      <Hero />
-      <Starter />
+     <div class="bg-red-500 flex  min-h-screen w-full justify-end">
+      <div class="bg-slate-500 grow flex flex-col px-10 gap-3">
+        
+        <div class="h-14 bg-slate-500 flex   justify-between  mt-3">
+            <div class="bg-white  h-full w-32 "></div>
+            <div class="bg-white  h-full w-32"></div>
+            <div class="bg-white  h-full w-32"></div>
+            <div class="bg-white  h-full w-32 "></div>
+            <div class="bg-white  h-full w-32 "></div>
+         </div>
 
-      <div role="presentation" class="ellipsis"></div>
-      <div role="presentation" class="ellipsis ellipsis-purple"></div>
-
-      <div class="container container-center container-spacing-xl">
-        <h3>
-          You can <span class="highlight">count</span>
-          <br /> on me
-        </h3>
-        <Counter />
-      </div>
-
-      <div class="container container-flex">
-        <Infobox>
-          <div q:slot="title" class="icon icon-cli">
-            CLI Commands
+         <div class="h-14 bg-slate-500 flex gap-4   ">
+            <div class="bg-white  grow "></div>
+            <div class="bg-white  h-full w-24"></div>
           </div>
-          <>
-            <p>
-              <code>npm run dev</code>
-              <br />
-              Starts the development server and watches for changes
-            </p>
-            <p>
-              <code>npm run preview</code>
-              <br />
-              Creates production build and starts a server to preview it
-            </p>
-            <p>
-              <code>npm run build</code>
-              <br />
-              Creates production build
-            </p>
-            <p>
-              <code>npm run qwik add</code>
-              <br />
-              Runs the qwik CLI to add integrations
-            </p>
-          </>
-        </Infobox>
 
-        <div>
-          <Infobox>
-            <div q:slot="title" class="icon icon-apps">
-              Example Apps
-            </div>
-            <p>
-              Have a look at the <a href="/demo/flower">Flower App</a> or the{" "}
-              <a href="/demo/todolist">Todo App</a>.
-            </p>
-          </Infobox>
+          <div class="h-14 bg-slate-500 flex   justify-between ">
+            <div class="bg-white  h-full w-32 "></div>
+            <div class="bg-white  h-full w-32"></div>
+            <div class="bg-white  h-full w-32"></div>
+            <div class="bg-white  h-full w-32 "></div>
+            <div class="bg-white  h-full w-32 "></div>
+            <div class="bg-white  h-full w-10 "></div>
+         </div>
 
-          <Infobox>
-            <div q:slot="title" class="icon icon-community">
-              Community
+         <div class="h-6 bg-slate-500 flex gap-4   ">
+            <div class="bg-white  grow "></div>
+            <div class="bg-white  h-full w-36"></div>
+            <div class="bg-white  h-full w-36"></div>
+          </div>
+          <div class="bg-white h-24 flex  1 rounded-3xl mt-7 ">
             </div>
-            <ul>
-              <li>
-                <span>Questions or just want to say hi? </span>
-                <a href="https://qwik.builder.io/chat" target="_blank">
-                  Chat on discord!
-                </a>
-              </li>
-              <li>
-                <span>Follow </span>
-                <a href="https://twitter.com/QwikDev" target="_blank">
-                  @QwikDev
-                </a>
-                <span> on Twitter</span>
-              </li>
-              <li>
-                <span>Open issues and contribute on </span>
-                <a href="https://github.com/BuilderIO/qwik" target="_blank">
-                  GitHub
-                </a>
-              </li>
-              <li>
-                <span>Watch </span>
-                <a href="https://qwik.builder.io/media/" target="_blank">
-                  Presentations, Podcasts, Videos, etc.
-                </a>
-              </li>
-            </ul>
-          </Infobox>
-        </div>
+            <div class="bg-white h-24 flex  1 rounded-3xl ">
+            </div>
+            <div class="bg-white h-24 flex  1 rounded-3xl ">
+            </div>
+            <div class="bg-white h-24 flex  1 rounded-3xl ">
+            </div>
+
+
+          
       </div>
+     
+      <div class="bg-white w-96 flex flex-col px-3 md:opacity-100 opacity-0">
+
+        <div class="  bg-white w-full flex gap-4 flex-col items-center mt-6">
+            <div class="bg-slate-500 h-12 w-52"></div>
+         </div>
+        
+        <div class="  bg-white w-full flex gap-4 flex-col items-center mt-16">
+            <div class="bg-slate-500 h-12 w-52"></div>
+            <div class="bg-slate-500 h-12 w-52"></div>
+         </div>
+
+         <div class="  bg-white w-full flex gap-4 flex-col items-center mt-28">
+            <div class="bg-slate-500 h-12 w-52"></div>
+            <div class="bg-slate-500 h-12 w-52"></div>
+            <div class="bg-slate-500 h-12 w-52"></div>
+            <div class="bg-slate-500 h-12 w-52"></div>
+            <div class="bg-slate-500 h-12 w-52"></div>
+            <div class="bg-slate-500 h-12 w-52"></div>
+         </div>
+
+         
+         
+
+      </div>
+
+        
+    </div>
     </>
   );
-});
-
-export const head: DocumentHead = {
-  title: "Welcome to Qwik",
-  meta: [
-    {
-      name: "description",
-      content: "Qwik site description",
-    },
-  ],
 };
+
+
