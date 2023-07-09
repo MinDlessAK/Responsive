f23bee4e455ffc7838c50a2d5b03722ad6cc9fc8
MinDlessAK <MinDlessAK@users.noreply.github.com>
2023-07-09 12:34:07 +0530

    index

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,72 +1,14 @@
+import Body from "~/components/body";
+import Sidebar from "~/components/sidebar";
+
 export default () => {
   return (
     <>
      <div class="bg-red-500 flex  min-h-screen w-full justify-end">
-      <div class="bg-slate-500 grow flex flex-col px-10 gap-3">
-        
-        <div class="h-14 bg-slate-500 flex   justify-between  mt-3">
-            <div class="bg-white  h-full w-32 "></div>
-            <div class="bg-white  h-full w-32"></div>
-            <div class="bg-white  h-full w-32"></div>
-            <div class="bg-white  h-full w-32 "></div>
-            <div class="bg-white  h-full w-32 "></div>
-         </div>
-
-         <div class="h-14 bg-slate-500 flex gap-4   ">
-            <div class="bg-white  grow "></div>
-            <div class="bg-white  h-full w-24"></div>
-          </div>
-
-          <div class="h-14 bg-slate-500 flex   justify-between ">
-            <div class="bg-white  h-full w-32 "></div>
-            <div class="bg-white  h-full w-32"></div>
-            <div class="bg-white  h-full w-32"></div>
-            <div class="bg-white  h-full w-32 "></div>
-            <div class="bg-white  h-full w-32 "></div>
-            <div class="bg-white  h-full w-10 "></div>
-         </div>
-
-         <div class="h-6 bg-slate-500 flex gap-4   ">
-            <div class="bg-white  grow "></div>
-            <div class="bg-white  h-full w-36"></div>
-            <div class="bg-white  h-full w-36"></div>
-          </div>
-          <div class="bg-white h-24 flex  1 rounded-3xl mt-7 ">
-            </div>
-            <div class="bg-white h-24 flex  1 rounded-3xl ">
-            </div>
-            <div class="bg-white h-24 flex  1 rounded-3xl ">
-            </div>
-            <div class="bg-white h-24 flex  1 rounded-3xl ">
-            </div>
-
-
-          
-      </div>
+      <Body></Body>
      
-      <div class="bg-white w-96 flex flex-col px-3 md:opacity-100 opacity-0">
-
-        <div class="  bg-white w-full flex gap-4 flex-col items-center mt-6">
-            <div class="bg-slate-500 h-12 w-52"></div>
-         </div>
-        
-        <div class="  bg-white w-full flex gap-4 flex-col items-center mt-16">
-            <div class="bg-slate-500 h-12 w-52"></div>
-            <div class="bg-slate-500 h-12 w-52"></div>
-         </div>
-
-         <div class="  bg-white w-full flex gap-4 flex-col items-center mt-28">
-            <div class="bg-slate-500 h-12 w-52"></div>
-            <div class="bg-slate-500 h-12 w-52"></div>
-            <div class="bg-slate-500 h-12 w-52"></div>
-            <div class="bg-slate-500 h-12 w-52"></div>
-            <div class="bg-slate-500 h-12 w-52"></div>
-            <div class="bg-slate-500 h-12 w-52"></div>
-         </div>
-
-         
-         
-
+      <div class="bg-white w-96 flex flex-col px-3">
+       <Sidebar></Sidebar>
       </div>
 
         
