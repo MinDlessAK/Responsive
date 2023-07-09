1709810a4fa1b5df03c1dc5ea068bbcbec655bb2
MinDlessAK <MinDlessAK@users.noreply.github.com>
2023-07-09 12:33:54 +0530

    body

---
+++ src/components/body.tsx
@@ -0,0 +1,45 @@
+export default function body() {
+  return (
+    <div class="bg-slate-500 grow flex flex-col px-10 gap-3">
+        
+        <div class="h-14 bg-slate-500 flex   justify-between  mt-3">
+            <div class="bg-white  h-full w-32 "></div>
+            <div class="bg-white  h-full w-32"></div>
+            <div class="bg-white  h-full w-32"></div>
+            <div class="bg-white  h-full w-32 "></div>
+            <div class="bg-white  h-full w-32 "></div>
+         </div>
+
+         <div class="h-14 bg-slate-500 flex gap-4   ">
+            <div class="bg-white  grow "></div>
+            <div class="bg-white  h-full w-24"></div>
+          </div>
+
+          <div class="h-14 bg-slate-500 flex   justify-between ">
+            <div class="bg-white  h-full w-32 "></div>
+            <div class="bg-white  h-full w-32"></div>
+            <div class="bg-white  h-full w-32"></div>
+            <div class="bg-white  h-full w-32 "></div>
+            <div class="bg-white  h-full w-32 "></div>
+            <div class="bg-white  h-full w-10 "></div>
+         </div>
+
+         <div class="h-6 bg-slate-500 flex gap-4   ">
+            <div class="bg-white  grow "></div>
+            <div class="bg-white  h-full w-36"></div>
+            <div class="bg-white  h-full w-36"></div>
+          </div>
+          <div class="bg-white h-24 flex  1 rounded-3xl mt-7 ">
+            </div>
+            <div class="bg-white h-24 flex  1 rounded-3xl ">
+            </div>
+            <div class="bg-white h-24 flex  1 rounded-3xl ">
+            </div>
+            <div class="bg-white h-24 flex  1 rounded-3xl ">
+            </div>
+
+
+          
+      </div>
+  )
+}
