812118e2256f74b2ca2f8b1f2a749a993cfde3e7
MinDlessAK <MinDlessAK@users.noreply.github.com>
2023-07-09 12:34:01 +0530

    sidebar

---
+++ src/components/sidebar.tsx
@@ -0,0 +1,27 @@
+
+
+export default function sidebar() {
+  return (
+    <div>
+         <div class="  bg-white w-full flex gap-4 flex-col items-center mt-6">
+            <div class="bg-slate-500 h-12 w-52"></div>
+         </div>
+        
+        <div class="  bg-white w-full flex gap-4 flex-col items-center mt-16">
+            <div class="bg-slate-500 h-12 w-52"></div>
+            <div class="bg-slate-500 h-12 w-52"></div>
+         </div>
+
+         <div class="  bg-white w-full flex gap-4 flex-col items-center mt-16">
+            <div class="bg-slate-500 h-12 w-52"></div>
+            <div class="bg-slate-500 h-12 w-52"></div>
+            <div class="bg-slate-500 h-12 w-52"></div>
+            <div class="bg-slate-500 h-12 w-52"></div>
+            <div class="bg-slate-500 h-12 w-52"></div>
+            <div class="bg-slate-500 h-12 w-52"></div>
+         </div>
+
+      
+    </div>
+  )
+}
